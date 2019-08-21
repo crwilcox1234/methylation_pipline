157c2889a28f4ff1ca81cd7e70c3513f6eb83e4c
Christina R. Wilcox <crwilcox@hpc-interactive-1-1.local>
2019-08-21 12:26:33 -0700

    adding merge python for after premerge

--- merge_sum1.py
+++ merge_sum1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 #outfile = open("merge-file-methyl-cov.bed",'w')
-outfile = open("test-file-methyl_heade.bed",'w')
+outfile = open("methyl_matrix_A_header.bed",'w')
 file1=open('list')
 row1=file1.readlines()
 print(len(row1))
@@ -10,13 +10,15 @@ for line in row1:
     line=line.split('\n')[0]
     a=line
     files.append(a)
-outfile.write(files[0]+'\t'+files[1]+'\t'+files[2]+'\t'+files[3]+'\t'+files[4]+'\t'+files[5]+'\t'+files[6]+'\t'+files[7]+'\t'+files[8]+'\t'+files[9]+'\t'+files[10]+'\t'+files[11]+'\t'+files[12]+'\t'+files[13]+'\t'+files[14]+'\t'+files[15]+'\t'+files[16]+'\t'+files[17]+'\t'+files[18]+'\t'+files[19]+'\t'+files[20]+'\t'+files[21]+'\t'+files[22]+'\t'+files[23]+'\t'+files[24]+'\t'+files[25]+'\t'+files[26]+'\t'+files[27]+'\t'+files[28]+'\t'+files[29]+'\t'+files[30]+'\t'+files[31]+'\t'+files[32]+'\t'+files[33]+'\t'+files[34]+'\t'+files[35]+'\t'+files[36]+'\t'+files[37]+'\t'+files[38]+'\t'+files[39]+'\n')
+outfile.write(files[0]+'\t'+files[1]+'\t'+files[2]+'\t'+files[3]+'\t'+files[4]+'\t'+files[5]+'\t'+files[6]+'\t'+files[7]+'\t'+files[8]+'\t'+files[9]+'\t'+files[10]+'\t'+files[11]+'\t'+files[12]+'\t'+files[13]+'\t'+files[14]+'\t'+files[15]+'\t'+files[16]+'\t'+files[17]+'\t'+files[18]+'\t'+files[19]+'\t'+files[20]+'\t'+'\n')
 
-print(files)
+#+files[22]+'\t'+files[23]+'\t'+files[24]+'\t'+files[25]+'\t'+files[26]+'\t'+files[27]+'\t'+files[28]+'\t'+files[29]+'\t'+files[30]+'\t'+files[31]+'\t'+files[32]+'\t'+files[33]+'\t'+files[34]+'\t'+files[35]+'\t'+files[36]+'\t'+files[37]+'\t'+files[38]+'\t'+files[39]+'\n')
+
+#print(len(files))
 
 
 
-dict={}
+#dict={}
 
 #list2='symbol'
 #for line in row1:
@@ -26,9 +28,9 @@ dict={}
 #outfile.write(list2)
 #print(list2)
 
-
 #testFile=open('merge_sig_diff_cut_8042.seq134.trim43_trimmed_bismark_bt2.bedGraph.gz.bismark.zero.cov','r')
-dictfile=open('everything_merge_uniq_addedrows.txt','r') #everything_merge_text is made using bedops --everything.  Use the output file from bedops --everything, do not remove extra columns
+dict={}
+dictfile=open('merge_A_abr_uniq.txt','r') #everything_merge_text is made using bedops --everything.  Use the output file from bedops --everything, use awk -F "\t" 'BEGIN {OFS="\t"} {print$1,$2,$3}' merge_A_total.cov | uniq > merge_A_abr_uniq.txt to only have uniq entries. 
 for line in dictfile:
     elem = line.strip().split('\t')
     dict[elem[0]+':'+elem[1]+':'+elem[2]]=[]
